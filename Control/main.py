f441646cc9314be090c1be704edc50ae51fc3b99
SevR0V <ermakovigorrov@gmail.com>
2024-10-23 23:06:59 +0300

    Update main.py

--- Control/main.py
+++ Control/main.py
@@ -28,7 +28,7 @@ trustersXValues = [-500, 500]
 
 #init control system
 controlSystem = HROVControlSystem(thrustersDirCorr)
-controlSystem.set_thrusters_calibration_values(thrustersOrder, trustersXValues, 0.5)
+controlSystem.set_thrusters_calibration_values(thrustersDirCorr, thrustersOrder, trustersXValues, 0.5)
 
 #init timer parameters
 timerInterval = 1/500 #500 Hz timer interval
